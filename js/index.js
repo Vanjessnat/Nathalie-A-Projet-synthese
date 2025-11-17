121137f9bcf2b2eebe3d20a6813f74a56d35b651
Vanjessnat <vanjessnat@gmail.com>
2025-11-17 15:16:17 -0500

    Importation des pages sur la branche apropos et ajout des sections page apropos

---
+++ js/index.js
@@ -0,0 +1,67 @@
+/* fonction qui révèle un texte  */
+
+function slideToggle() {
+  let box = document.getElementById('box');
+  if (box.style.maxHeight && box.style.maxHeight !== '0px') {
+    box.style.maxHeight = '0';
+  } else {
+    box.style.maxHeight = box.scrollHeight + 'px';
+  }
+}
+
+/* Contenu section glisse de gauche à droite  */
+
+let sliders = document.querySelectorAll('.slide-in-left');
+
+let observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.3
+});
+
+sliders.forEach(slide => {
+  observer.observe(slide);
+});
+
+/* Apparition des logos seulement quand la section arrive à l’écran  */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const logos = document.querySelectorAll(".ref-logo");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                // Optionnel : arrêter d'observer après apparition
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.3 // déclenche à 30% visible
+    });
+
+    logos.forEach(logo => observer.observe(logo));
+});
+
+/* fonction afficher une alerte et changer de couleur  */
+
+let monBouton = document.getElementById("bouton-alerte");
+monBouton.addEventListener("mouseover", () => changerCouleurDuBouton(monBouton, "#a39b3dff"));
+monBouton.addEventListener("mouseout", () => changerCouleurDuBouton(monBouton, "#e0ebd5ff"));
+monBouton.addEventListener("click", () => afficherUneAlerte());
+
+
+function changerCouleurDuBouton(unBouton, couleur) {
+  unBouton.setAttribute("style", "background-color:" + couleur);
+}
+
+function afficherUneAlerte() { alert("Appelez-nous sans plus tarder au 450 756-6363 ou présentez-vous au : 24 rue Hubert Joliette J6E 6R5 !"); }
+
+
+
